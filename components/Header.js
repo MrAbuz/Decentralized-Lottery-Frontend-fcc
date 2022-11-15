03941b26c293f8ba62b249b3b486e61b508f9890
MrAbuz <miguelmrosario@gmail.com>
2022-11-15 18:11:07 +0000

    15/11/2022

--- components/Header.js
+++ components/Header.js
@@ -8,9 +8,24 @@ import { ConnectButton } from "web3uikit"
 //"moralisAuth={false}" again just making clear we're not trying to connect to a server
 export default function Header() {
     return (
-        <div>
-            Decentralized Lottery
-            <ConnectButton moralisAuth={false} />
+        <div className="p-5 border-b-2 flex flex-row">
+            <h1 className="py-4 px-4 font-semibold text-3xl">Decentralized Lottery</h1>
+            <div className="ml-auto py-2 px-4">
+                <ConnectButton moralisAuth={false} />
+            </div>
         </div>
     )
 }
+
+//                  CSS/Tailwind explanation from trial and error:
+//p-5 adds 5 pixels in the upper part and lower part of the header (or maybe to every side)
+//flex makes the button be in the same line as the title "Decentralized Lottery"
+//flex-row didnt change anything but must be related to the flex
+//<h1></h1> makes "Decentralized Lottery" the "header 1", in order to add a className to it and format it
+//py-4 added some padding to the top(space between "Decentralized Lottery" and the top).But only in "Decentralized Lottery",the button kept the overall padding of the div
+//px-4 padding in the left side (x axis)
+//font-bold changes the text to bold (I chose font-semibold because I liked it)
+//text-3xl increases the text
+//ml-auto -> automatic left margin (means that it will be max spaced to the right doesnt matter the screen size? didnt understand 100%, but pushed max to the right)(looks super nice to use)
+//py-2 some padding relative to the top
+//px-4 added some padding to the right. interesting that now since the button is in the right, the x padding adds to the right. In the tittle it added padding in the left
--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -28,7 +28,11 @@ export default function LotteryEntrance() {
     const dispatch = useNotification() //this dispatch is that little popup notification. Had to import and add it as a wrapped around component in the app.js file
 
     //runContractFunction, which we renamed to enterRaffle, its to include in the html to call this function
-    const { runContractFunction: enterRaffle } = useWeb3Contract({
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi, //abi doesnt change no matter the network we're on
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
@@ -85,6 +89,7 @@ export default function LotteryEntrance() {
 
     const handleSuccess = async function (tx) {
         //takes the transaction as an input parameter
+        //the onSuccess parameter isnt checking that the transaction has a block confirmation, only that it was sent. Thats why we add tx.wait(1)
         await tx.wait(1)
         handleNewNotification(tx)
         updateUI() //super important.when the transaction goes through,the number of players is not updated cuz it doesnt render.so after .wait(1) we add this updateUI(). So nice!
@@ -97,7 +102,7 @@ export default function LotteryEntrance() {
         dispatch({
             type: "info",
             message: "Transaction Complete!",
-            title: "Tx Notification",
+            title: "Notification",
             position: "topR",
             icon: "bell",
         })
@@ -107,20 +112,29 @@ export default function LotteryEntrance() {
     //{raffleAddress ? : } -> we can only call the function button and see the entrance fee, as long as there is a raffle address, which makes sense.
     //we gave the "onSuccess" parameter to the enterRaffle(). This functions come with "onComplete", "onError" etc, super useful!
     //we should always add onError to call our runContractFunction calls, because if any of those calls break we wont know if we dont add it!
+    //And the onSuccess isnt checking that the transaction has a block confirmation, only that it was sent. Thats why in "handleSuccess" we add tx.wait(1)
+    // {isLoading || isFetching ? <div></div> : <div><div>} -> if its loading or fetching we add the spinny thing, if not just the text.
+    // Nice that you can add logic inside <button></button> where it was suposed to be just the name of the button, previously it was just "Enter Raffle" text
     return (
-        <div>
+        <div className="p-5">
             Hi from Lottery entrance!
             {raffleAddress ? (
-                <div>
+                <div className="">
                     <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded ml-auto"
                         onClick={async function () {
                             await enterRaffle({
                                 onSuccess: handleSuccess,
                                 onError: (error) => console.log(error),
                             })
                         }}
+                        disabled={isLoading || isFetching}
                     >
-                        Enter Raffle
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            <div>Enter Raffle</div>
+                        )}
                     </button>
                     Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH Players:
                     {numPlayers}
@@ -141,4 +155,33 @@ export default function LotteryEntrance() {
 //so, after the contract (that was changed) gets deployed, it automatically runs the script that creates the constants folder for us here in the frontend
 
 // (*) https://web3ui.github.io/web3uikit/?path=/story/1-web3-parse-blockie--custom-seed
+// explore the website to find nice thing by visualizing them, and then I have the code aswell
 // in the left menu -> 5 POPUP -> notification -> hook Demo; then "docs" above; then "show code" in the right
+
+//                  CSS/Tailwind explanation from trial and error:
+//p-5; adds padding to the top and left side (or maybe every side, but can only notice top and left because its on the top left)
+//bg-blue-500; changes the background (bg) to the colour blue-500
+//hover:bg-blue-700; when we hover over it, it turns into colour blue-700 (nice)
+//text-white; text is white
+//font-bold; but I used font-semibold because it looks better
+//py-2; some y padding (padding inside the button, made the button larger top and bottom)
+//px-4; some x padding (pading inside the button, made the button larger right and left)
+//rounded; made the buttom be rounded, rounded corners
+//ml-auto (margin left auto) in the connect button it pushed to the max right side as possible (even in different screen sizes I think). here it didnt change anything
+//animate-spin spinner-border h-8 w-8 border-b-2 rounded-full; it adds the spinny thing, mostly works together but I can try one by one to see the differences
+
+//Patrick challenge:
+//Calling is to listen to event of the winner and instantly render the winning address:
+//Patrick gave us a challenge in the end to find a way to listen to the event we emit when we find a winner, and when we listen to it, update the address of the
+//winner in the UI. Because atm it updates the address of the winner if we refresh, but it doesnt render in the moment that it finds a winner.
+//Based on my thoughts until now, I think it would be something like this:
+//
+//    useEffect(() => {
+//        if (isWeb3Enabled) {
+//            updateUI() //or only "setRecentWinner(recentWinnerFromCall)" + "const recentWinnerFromCall = await getRecentWinner(onError etc)"
+//        }
+//    }, Raffle.WinnerPicked()) //this is obviously not the way we listen to the event. Had to have the Raffle contract object defined aswell.
+//
+//We can also listen to the event of RaffleEnter and update the frontend when some user enters the lottery.
+//Need to learn how to listen to events
+//If Patrick doesn't explain in the end, i'll try to figure out and add this event to render the address!
